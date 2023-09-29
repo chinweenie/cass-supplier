897fb6793130b626bf9447f4ca5ab6b04b26b90f
chinweenie <chinweenie@istox.com>
2023-09-29 15:42:07 +0800

    Update data modeling in startup script

--- data_files/initialize_csv_files.py
+++ data_files/initialize_csv_files.py
@@ -2,7 +2,6 @@ import json
 
 import pandas
 
-
 def merge_name(row):
     return {
         'first': row['c_first'],
@@ -23,7 +22,7 @@ def merge_cells(row):
 
 
 def process_warehouse():
-    warehouse_df = pandas.read_csv('warehouse.csv', dtype=str)
+    warehouse_df = pandas.read_csv('warehouse.csv', dtype=str, header=None)
     warehouse_df.columns = ['w_id', 'w_name', 'street_1', 'street_2', 'city', 'state', 'zip', 'w_tax',
                             'w_ytd']
     warehouse_df['w_address'] = warehouse_df.apply(merge_cells, axis=1)
@@ -33,7 +32,7 @@ def process_warehouse():
 
 
 def process_district():
-    district_df = pandas.read_csv('district.csv')
+    district_df = pandas.read_csv('district.csv',header=None)
     district_df.columns = ['d_w_id', 'd_id', 'd_name', 'street_1', 'street_2', 'city', 'state', 'zip',
                            'd_tax', 'd_ytd', 'd_next_o_id']
     district_df['address'] = district_df.apply(merge_cells, axis=1)
@@ -41,24 +40,33 @@ def process_district():
     district_df.to_csv('district_df.csv',index=False)
 
 def process_items():
-    item_df = pandas.read_csv('item.csv')
+    item_df = pandas.read_csv('item.csv', header=None)
     item_df.columns = ['i_id', 'i_name', 'i_price', 'i_im_id', 'i_data']
     item_df.to_csv('item_df.csv',index=False)
 
 def process_order():
-    order_df = pandas.read_csv('order.csv')
-    order_df.columns = ['o_w_id', 'o_d_id', 'o_id', 'o_c_id', 'o_carrier_id', 'o_ol_cnt', 'o_all_local', 'o_entry_d']
+    order_df = pandas.read_csv('order.csv', header=None)
+    order_df.columns = ['o_w_id', 'o_d_id', 'o_id', 'o_c_id', 'o_ol_cnt','o_carrier_id', 'o_all_local', 'o_entry_d']
     order_df.to_csv('order_df.csv',index=False)
 
+def process_orders_by_customer():
+    orders_df = pandas.read_csv('order_df.csv')
+    customers_df = pandas.read_csv('customer_df.csv')
+
+    orders_by_customer_df = pandas.merge(customers_df, orders_df,
+                                         left_on=['c_w_id', 'c_d_id', 'c_id'],
+                                         right_on=['o_w_id', 'o_d_id', 'o_c_id'],
+                                         how='inner')
+    orders_by_customer_df = orders_by_customer_df.loc[:, ['c_w_id', 'c_d_id', 'c_id', 'o_id', 'o_carrier_id', 'o_entry_d']]
+    orders_by_customer_df.to_csv('orders_by_customer_df.csv', index=False)
+
 def process_order_line():
-    df = pandas.read_csv('order_line_df.csv')
-    print(df.shape)
-    # order_line_df = pandas.read_csv('order-line.csv')
-    # order_line_df.columns = ['ol_w_id', 'ol_d_id', 'ol_o_id', 'ol_number', 'ol_i_id', 'ol_delivery_d', 'ol_amount', 'ol_supply_w_id', 'ol_quantity', 'ol_dist_info']
-    # order_line_df.to_csv('order_line_df.csv',index=False)
+    order_line_df = pandas.read_csv('order-line.csv', header=None)
+    order_line_df.columns = ['ol_w_id', 'ol_d_id', 'ol_o_id', 'ol_number', 'ol_i_id', 'ol_delivery_d', 'ol_amount', 'ol_supply_w_id', 'ol_quantity', 'ol_dist_info']
+    order_line_df.to_csv('order_line_df.csv',index=False)
 
 def process_stock():
-    stock_df = pandas.read_csv('stock.csv')
+    stock_df = pandas.read_csv('stock.csv', header=None)
     stock_df.columns = ['s_w_id', 's_i_id', 's_quantity', 's_ytd', 's_order_cnt','s_remote_cnt',
                         's_dist_01', 's_dist_02', 's_dist_03', 's_dist_04', 's_dist_05', 's_dist_06', 's_dist_07', 's_dist_08',
                         's_dist_09', 's_dist_10', 's_data'
@@ -66,7 +74,7 @@ def process_stock():
     stock_df.to_csv('stock_df.csv',index=False)
 
 def process_customer():
-    customer_df = pandas.read_csv('customer.csv')
+    customer_df = pandas.read_csv('customer.csv', header=None)
     customer_df.columns = ['c_w_id', 'c_d_id', 'c_id', 'c_first', 'c_middle', 'c_last',
                            'street_1', 'street_2', 'city',
                            'state', 'zip',
@@ -77,10 +85,17 @@ def process_customer():
     customer_df['c_address'] = customer_df.apply(merge_cells, axis=1)
     customer_df['c_name'] = customer_df.apply(merge_name, axis=1)
     customer_df = customer_df.drop(['c_first', 'c_middle', 'c_last', 'street_1', 'street_2', 'city', 'state', 'zip'], axis=1)
-    # row_counts = customer_df.count(axis=1)
-
-    # print(row_counts)
     customer_df.to_csv('customer_df.csv',index=False)
+
+    top_balances_df = pandas.DataFrame({
+        'c_id': [],
+        'c_w_id': [],
+        'c_d_id': [],
+        'c_balance': []
+    })
+    top_balances_df[['c_id', 'c_w_id', 'c_d_id', 'c_balance']] = customer_df[['c_id', 'c_w_id', 'c_d_id', 'c_balance']]
+    top_balances_df.to_csv('top_balances_df.csv', index=False)
+
 
 
 if __name__ == '__main__':
@@ -91,4 +106,5 @@ if __name__ == '__main__':
     process_order_line()
     process_stock()
     process_customer()
+    process_orders_by_customer()
 
