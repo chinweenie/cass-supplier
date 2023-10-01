e0dd2b00121153a6eb4199c6ec2bb267980a85ad
Cyolune <jan.alfenson.tan@gmail.com>
2023-10-02 00:50:47 +0800

    Add processed table for txn 2.8

--- initialize_csv_files.py
+++ initialize_csv_files.py
@@ -52,10 +52,9 @@ def process_order():
 
 def process_order_line():
     df = pandas.read_csv('order-line.csv', header=None)
-    #print(df.shape)
-    # order_line_df = pandas.read_csv('order-line.csv')
-    # order_line_df.columns = ['ol_w_id', 'ol_d_id', 'ol_o_id', 'ol_number', 'ol_i_id', 'ol_delivery_d', 'ol_amount', 'ol_supply_w_id', 'ol_quantity', 'ol_dist_info']
-    # order_line_df.to_csv('order_line_df.csv',index=False)
+    order_line_df = pandas.read_csv('order-line.csv')
+    order_line_df.columns = ['ol_w_id', 'ol_d_id', 'ol_o_id', 'ol_number', 'ol_i_id', 'ol_delivery_d', 'ol_amount', 'ol_supply_w_id', 'ol_quantity', 'ol_dist_info']
+    order_line_df.to_csv('order_line_df.csv',index=False)
 
 def process_stock():
     stock_df = pandas.read_csv('stock.csv', header=None)
@@ -77,9 +76,6 @@ def process_customer():
     customer_df['c_address'] = customer_df.apply(merge_cells, axis=1)
     customer_df['c_name'] = customer_df.apply(merge_name, axis=1)
     customer_df = customer_df.drop(['c_first', 'c_middle', 'c_last', 'street_1', 'street_2', 'city', 'state', 'zip'], axis=1)
-    # row_counts = customer_df.count(axis=1)
-
-    # print(row_counts)
     customer_df.to_csv('customer_df.csv',index=False)
 
 def process_undelivered_orders_by_warehouse_district():
@@ -90,9 +86,53 @@ def process_undelivered_orders_by_warehouse_district():
 
     undelivered_orders_by_wd_df.to_csv('undelivered_orders.csv', index=False)
 
+def process_related_customers_txns():
+    orders_df = pandas.read_csv('order_df.csv')
+    orders_df = orders_df.drop(['o_carrier_id', 'o_ol_cnt', 'o_all_local', 'o_entry_d'], axis=1)
 
-if __name__ == '__main__':
+    order_line_df = pandas.read_csv('order_line_df.csv')
+    order_line_df = order_line_df.drop(['ol_delivery_d', 'ol_amount', 'ol_supply_w_id', 'ol_quantity', 'ol_dist_info'], axis=1)
+
+    # rename columns in order and order_line for inner join
+    orders_df.rename(columns={'o_w_id': 'w_id', 'o_d_id': 'd_id'}, inplace=True)
+    order_line_df.rename(columns={'ol_w_id': 'w_id', 'ol_d_id': 'd_id', 'ol_o_id': 'o_id'}, inplace=True)
+
+    order_join_orderline_df = pandas.merge(orders_df, order_line_df, on=['w_id', 'd_id', 'o_id'])
+
+    print('order_join_orderline_df size: ' + str(order_join_orderline_df.shape))
+    order_join_orderline_df.to_csv('order_join_orderline_df.csv', index=False)
+
     '''
+    two_item_column_names = ['w_id', 'd_id', 'o_id', 'c_id', 'ol_number', 'i1_id', 'i2_id']
+    two_item_df = pandas.DataFrame(columns=two_item_column_names)
+    grouped = order_join_orderline_df.groupby(['w_id', 'd_id', 'o_id', 'o_c_id'])
+
+    group_count_tracker = 0
+
+    for name, group in grouped:
+        group_copy = group.copy()
+        group_copy.rename(columns={'w_id': 'w_id1', 'd_id': 'd_id1', 'o_id': 'o_id1','o_c_id': 'o_c_id1', 'ol_number': 'ol_number1', 'ol_i_id': 'ol_i_id1'}, inplace=True)
+        cross_df = pandas.merge(group, group_copy, how='cross')
+
+        item_filter = (cross_df['ol_i_id'] != cross_df['ol_i_id1'])
+        cross_df = cross_df[item_filter]
+
+        cross_df = cross_df.drop(['w_id1', 'd_id1', 'o_id1', 'o_c_id1', 'ol_number1'], axis=1)
+        cross_df.rename(columns={'o_c_id': 'c_id', 'ol_i_id': 'i1_id', 'ol_i_id1': 'i2_id'}, inplace=True)
+
+        cross_df = cross_df.reset_index(drop=True)
+        two_item_df = two_item_df.reset_index(drop=True)
+        two_item_df = pandas.concat([two_item_df, cross_df], axis=0, ignore_index=True)
+        
+        group_count_tracker += 1
+        print("% processed: " + str(group_count_tracker / grouped.ngroups))
+    
+    print('two_item_df.shape: ' + str(two_item_df.shape))
+    two_item_df.to_csv('two_item_df.csv', index=False)
+    '''
+    
+if __name__ == '__main__':
+    
     process_warehouse()
     process_district()
     process_items()
@@ -100,8 +140,7 @@ if __name__ == '__main__':
     process_order_line()
     process_stock()
     process_customer()
-    '''
-
     process_undelivered_orders_by_warehouse_district()
+    process_related_customers_txns()
     
 
