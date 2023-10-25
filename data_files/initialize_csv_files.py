696d05d8376b3a210233e4c17c7b8e8413bedf35
chinweenie <chinweenie@istox.com>
2023-10-26 01:54:15 +0800

    Fix directory

--- data_files/initialize_csv_files.py
+++ data_files/initialize_csv_files.py
@@ -22,58 +22,58 @@ def merge_cells(row):
 
 
 def process_warehouse():
-    warehouse_df = pandas.read_csv('warehouse.csv', dtype=str, header=None)
+    warehouse_df = pandas.read_csv('/temp/teamd-cass/data_files/warehouse.csv', dtype=str, header=None)
     warehouse_df.columns = ['w_id', 'w_name', 'street_1', 'street_2', 'city', 'state', 'zip', 'w_tax',
                             'w_ytd']
     warehouse_df['w_address'] = warehouse_df.apply(merge_cells, axis=1)
     warehouse_df = warehouse_df.drop(['street_1', 'street_2', 'city', 'state', 'zip'], axis=1)
-    warehouse_df.to_csv('warehouse_df.csv',index=False, sep=',')
+    warehouse_df.to_csv('/temp/teamd-cass/data_files/warehouse_df.csv',index=False, sep=',')
 
 
 def process_district():
-    district_df = pandas.read_csv('district.csv',header=None)
+    district_df = pandas.read_csv('/temp/teamd-cass/data_files/district.csv',header=None)
     district_df.columns = ['d_w_id', 'd_id', 'd_name', 'street_1', 'street_2', 'city', 'state', 'zip',
                            'd_tax', 'd_ytd', 'd_next_o_id']
     district_df['address'] = district_df.apply(merge_cells, axis=1)
     district_df = district_df.drop(['street_1', 'street_2', 'city', 'state', 'zip'], axis=1)
-    district_df.to_csv('district_df.csv',index=False)
+    district_df.to_csv('/temp/teamd-cass/data_files/district_df.csv',index=False)
 
 def process_items():
-    item_df = pandas.read_csv('item.csv', header=None)
+    item_df = pandas.read_csv('/temp/teamd-cass/data_files/item.csv', header=None)
     item_df.columns = ['i_id', 'i_name', 'i_price', 'i_im_id', 'i_data']
-    item_df.to_csv('item_df.csv',index=False)
+    item_df.to_csv('/temp/teamd-cass/data_files/item_df.csv',index=False)
 
 def process_order():
-    order_df = pandas.read_csv('order.csv', header=None)
+    order_df = pandas.read_csv('/temp/teamd-cass/data_files/order.csv', header=None)
     order_df.columns = ['o_w_id', 'o_d_id', 'o_id', 'o_c_id', 'o_carrier_id', 'o_ol_cnt', 'o_all_local', 'o_entry_d']
-    order_df.to_csv('order_df.csv',index=False)
+    order_df.to_csv('/temp/teamd-cass/data_files/order_df.csv',index=False)
 
 def process_orders_by_customer():
-    orders_df = pandas.read_csv('order_df.csv')
-    customers_df = pandas.read_csv('customer_df.csv')
+    orders_df = pandas.read_csv('/temp/teamd-cass/data_files/order_df.csv')
+    customers_df = pandas.read_csv('/temp/teamd-cass/data_files/customer_df.csv')
 
     orders_by_customer_df = pandas.merge(customers_df, orders_df,
                                          left_on=['c_w_id', 'c_d_id', 'c_id'],
                                          right_on=['o_w_id', 'o_d_id', 'o_c_id'],
                                          how='inner')
     orders_by_customer_df = orders_by_customer_df.loc[:, ['c_w_id', 'c_d_id', 'c_id', 'o_id', 'o_carrier_id', 'o_entry_d']]
-    orders_by_customer_df.to_csv('orders_by_customer_df.csv', index=False)
+    orders_by_customer_df.to_csv('/temp/teamd-cass/data_files/orders_by_customer_df.csv', index=False)
 
 def process_order_line():
-    order_line_df = pandas.read_csv('order-line.csv', header=None)
+    order_line_df = pandas.read_csv('/temp/teamd-cass/data_files/order-line.csv', header=None)
     order_line_df.columns = ['ol_w_id', 'ol_d_id', 'ol_o_id', 'ol_number', 'ol_i_id', 'ol_delivery_d', 'ol_amount', 'ol_supply_w_id', 'ol_quantity', 'ol_dist_info']
-    order_line_df.to_csv('order_line_df.csv',index=False)
+    order_line_df.to_csv('/temp/teamd-cass/data_files/order_line_df.csv',index=False)
 
 def process_stock():
-    stock_df = pandas.read_csv('stock.csv', header=None)
+    stock_df = pandas.read_csv('/temp/teamd-cass/data_files/stock.csv', header=None)
     stock_df.columns = ['s_w_id', 's_i_id', 's_quantity', 's_ytd', 's_order_cnt','s_remote_cnt',
                         's_dist_01', 's_dist_02', 's_dist_03', 's_dist_04', 's_dist_05', 's_dist_06', 's_dist_07', 's_dist_08',
                         's_dist_09', 's_dist_10', 's_data'
                         ]
-    stock_df.to_csv('stock_df.csv',index=False)
+    stock_df.to_csv('/temp/teamd-cass/data_files/stock_df.csv',index=False)
 
 def process_customer():
-    customer_df = pandas.read_csv('customer.csv', header=None)
+    customer_df = pandas.read_csv('/temp/teamd-cass/data_files/customer.csv', header=None)
     customer_df.columns = ['c_w_id', 'c_d_id', 'c_id', 'c_first', 'c_middle', 'c_last',
                            'street_1', 'street_2', 'city',
                            'state', 'zip',
@@ -84,20 +84,20 @@ def process_customer():
     customer_df['c_address'] = customer_df.apply(merge_cells, axis=1)
     customer_df['c_name'] = customer_df.apply(merge_name, axis=1)
     customer_df = customer_df.drop(['c_first', 'c_middle', 'c_last', 'street_1', 'street_2', 'city', 'state', 'zip'], axis=1)
-    customer_df.to_csv('customer_df.csv',index=False)
+    customer_df.to_csv('/temp/teamd-cass/data_files/customer_df.csv',index=False)
 
 
 def process_top_balances():
-    customer_df = pandas.read_csv('customer_df.csv').loc[:, ['c_id', 'c_w_id', 'c_d_id', 'c_balance', 'c_name']]
-    warehouses_df = pandas.read_csv('warehouse_df.csv').loc[:, ['w_id', 'w_name']]
-    districts_df = pandas.read_csv('district_df.csv').loc[:, ['d_w_id','d_id', 'd_name']]
+    customer_df = pandas.read_csv('/temp/teamd-cass/data_files/customer_df.csv').loc[:, ['c_id', 'c_w_id', 'c_d_id', 'c_balance', 'c_name']]
+    warehouses_df = pandas.read_csv('/temp/teamd-cass/data_files/warehouse_df.csv').loc[:, ['w_id', 'w_name']]
+    districts_df = pandas.read_csv('/temp/teamd-cass/data_files/district_df.csv').loc[:, ['d_w_id','d_id', 'd_name']]
     merged_df = pandas.merge(customer_df, districts_df, left_on=['c_d_id', 'c_w_id'], right_on=['d_id', 'd_w_id'], how='inner')
     final_merged_df = pandas.merge(merged_df, warehouses_df, left_on=['c_w_id'], right_on=['w_id'], how='inner')
     final_merged_df['dummy_partition_key'] = 'global'
     final_merged_df = final_merged_df.drop('d_id', axis=1)
     final_merged_df = final_merged_df.drop('d_w_id', axis=1)
     final_merged_df = final_merged_df.drop('w_id', axis=1)
-    final_merged_df.to_csv('top_balances_df.csv', index=False)
+    final_merged_df.to_csv('/temp/teamd-cass/data_files/top_balances_df.csv', index=False)
 
 def check_rows():
     customer_rows = pandas.read_csv('customer.csv').shape[0]
@@ -111,19 +111,19 @@ def check_rows():
 
 # for txn 2.3
 def process_undelivered_orders_by_warehouse_district():
-    orders_by_wd_df = pandas.read_csv('order_df.csv')
+    orders_by_wd_df = pandas.read_csv('/temp/teamd-cass/data_files/order_df.csv')
     is_undelivered = orders_by_wd_df['o_carrier_id'].isnull()
     undelivered_orders_by_wd_df = orders_by_wd_df[is_undelivered]
     undelivered_orders_by_wd_df = undelivered_orders_by_wd_df.drop(['o_ol_cnt', 'o_all_local', 'o_entry_d'], axis=1)
 
-    undelivered_orders_by_wd_df.to_csv('undelivered_orders.csv', index=False)
+    undelivered_orders_by_wd_df.to_csv('/temp/teamd-cass/data_files/undelivered_orders.csv', index=False)
 
 # for txn 2.8
 def process_related_customers_txns():
-    orders_df = pandas.read_csv('order_df.csv')
+    orders_df = pandas.read_csv('/temp/teamd-cass/data_files/order_df.csv')
     orders_df = orders_df.drop(['o_carrier_id', 'o_ol_cnt', 'o_all_local', 'o_entry_d'], axis=1)
 
-    order_line_df = pandas.read_csv('order_line_df.csv')
+    order_line_df = pandas.read_csv('/temp/teamd-cass/data_files/order_line_df.csv')
     order_line_df = order_line_df.drop(['ol_delivery_d', 'ol_amount', 'ol_supply_w_id', 'ol_quantity', 'ol_dist_info'], axis=1)
 
     # rename columns in order and order_line for inner join
@@ -139,7 +139,7 @@ def process_related_customers_txns():
     orders_by_warehouse_district_customer_df = orders_by_warehouse_district_customer_df.sort_values(by=['w_id', 'd_id', 'c_id'])
 
     print('orders_by_warehouse_district_customer_df size: ' + str(orders_by_warehouse_district_customer_df.shape))
-    orders_by_warehouse_district_customer_df.to_csv('orders_by_warehouse_district_customer_df.csv', index=False)
+    orders_by_warehouse_district_customer_df.to_csv('/temp/teamd-cass/data_files/orders_by_warehouse_district_customer_df.csv', index=False)
 
     '''
     two_item_column_names = ['w_id', 'd_id', 'o_id', 'c_id', 'ol_number', 'i1_id', 'i2_id']
@@ -172,8 +172,8 @@ def process_related_customers_txns():
 
 # for txn 2.5
 def process_storage_under_threshold():
-    order_line_df = pandas.read_csv("order_line_df.csv")
-    stock_df = pandas.read_csv("stock_df.csv")
+    order_line_df = pandas.read_csv("/temp/teamd-cass/data_files/order_line_df.csv")
+    stock_df = pandas.read_csv("/temp/teamd-cass/data_files/stock_df.csv")
 
     stock_df = stock_df[['s_w_id', 's_i_id', 's_quantity']].rename(
         columns={'s_w_id': 'w_id', 's_i_id': 'i_id'})
@@ -181,15 +181,15 @@ def process_storage_under_threshold():
         columns={'ol_w_id': 'w_id', 'ol_d_id': 'd_id', 'ol_i_id': 'i_id'})
 
     df = pandas.merge(order_line_df, stock_df, how='inner')
-    df.to_csv('storage_under_treshold.csv', index=False)
+    df.to_csv('/temp/teamd-cass/data_files/storage_under_treshold.csv', index=False)
 
 # for txn 2.6
 def process_popular_items():
-    district_df = pandas.read_csv("district_df.csv")
-    order_df = pandas.read_csv("order_df.csv")
-    customer_df = pandas.read_csv("customer_df.csv")
-    item_df = pandas.read_csv("item_df.csv")
-    order_line_df = pandas.read_csv("order_line_df.csv")
+    district_df = pandas.read_csv("/temp/teamd-cass/data_files/district_df.csv")
+    order_df = pandas.read_csv("/temp/teamd-cass/data_files/order_df.csv")
+    customer_df = pandas.read_csv("/temp/teamd-cass/data_files/customer_df.csv")
+    item_df = pandas.read_csv("/temp/teamd-cass/data_files/item_df.csv")
+    order_line_df = pandas.read_csv("/temp/teamd-cass/data_files/order_line_df.csv")
 
     district_df = district_df.loc[:, ['d_w_id', 'd_id', 'd_next_o_id']].rename(
     columns={'d_w_id': 'w_id'})
@@ -215,7 +215,7 @@ def process_popular_items():
     df = pandas.merge(df, popular_order_line_df, how='inner')
     df = pandas.merge(df, item_df, how='inner')
     df = df.drop(['c_id', 'Rank', 'd_next_o_id'], axis=1)
-    df.to_csv('popular_items_table.csv', index=False)
+    df.to_csv('/temp/teamd-cass/data_files/popular_items_table.csv', index=False)
 
 if __name__ == '__main__':
     #check_rows()   
